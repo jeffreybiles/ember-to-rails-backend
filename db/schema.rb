01175d98188ee7b542fd6a891ce558436abb36d0
jeffreybiles <bilesjeffrey@gmail.com>
2016-10-30 15:03:56 -0500

    Active boolean attribute

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161025235751) do
+ActiveRecord::Schema.define(version: 20161030182905) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
 
   create_table "monsters", force: :cascade do |t|
     t.string  "name"
     t.integer "level"
+    t.boolean "active"
   end
 
 end
